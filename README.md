d0e2a8e8121d436117439063319c366244afabe5
sumit <sumit@pop-os.localdomain>
2021-05-02 22:45:32 +0530

    initial app

---
+++ README.md
@@ -0,0 +1,4 @@
+# tut_3
+
+## Navigator
+app maintain a stack of pages. when we route to one page we add it to the stack and when we press back we pop the current item from the stack.
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import './categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.black,
+        canvasColor: Color.fromRGBO(240, 240, 240, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.w300),
+            ),
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
