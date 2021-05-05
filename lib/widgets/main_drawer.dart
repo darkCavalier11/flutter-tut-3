609e1a8504c5ff644056150b8460326f4904fb52
sumit <sumit@pop-os.localdomain>
2021-05-05 19:05:49 +0530

    different type of push for diff sceario

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -39,10 +39,11 @@ class MainDrawer extends StatelessWidget {
             height: 20,
           ),
           buildListTile('Meals', Icons.restaurant, () {
-            Navigator.of(context).pushNamed('/');
+            // *replace the current page. useful for logins
+            Navigator.of(context).pushReplacementNamed('/');
           }),
           buildListTile('Filters', Icons.settings, () {
-            Navigator.of(context).pushNamed(FiltersScreen.routeName);
+            Navigator.of(context).pushReplacementNamed(FiltersScreen.routeName);
           }),
         ],
       ),
